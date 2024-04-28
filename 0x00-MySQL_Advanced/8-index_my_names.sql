03bf0702151af7a0ffa0d9ee84e47d5985e38310
LeaderSteve84 <mailstephenreal@gmail.com>
2024-04-28 09:24:51 +0000

    task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,3 @@
 -- script that creates an index idx_name_first on the
 -- table names and the first letter of name.
-ALTER TABLE names ADD COLUMN name_first_char CHAR(1)
-GENERATED ALWAYS AS (LEFT(name, 1));
-CREATE INDEX idx_name_first ON names (name);
+CREATE INDEX idx_name_first ON names ((LEFT(name, 1)));
