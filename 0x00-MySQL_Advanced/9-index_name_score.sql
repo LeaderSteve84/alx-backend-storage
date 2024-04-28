6f7c22a7895bfefd23e0ea76ff1ec1914f4763ca
LeaderSteve84 <mailstephenreal@gmail.com>
2024-04-28 10:46:00 +0000

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 -- on the table names and the first letter of name and the score.
 ALTER TABLE names ADD COLUMN name_first_char CHAR(1)
 GENERATED ALWAYS AS (LEFT(name, 1));
-CREATE INDEX idx_name_first_score ON names ( name_first_char, score);
+CREATE INDEX idx_name_first_score ON names ( name, score);
